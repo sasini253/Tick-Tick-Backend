83d8f4b53f5a94eadef7047a4f4a209cd9aed718
Sasini Maheesha <sashkavindi9819@gmail.com>
2021-12-02 22:34:28 +0530

    added

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-var express = require("express");
-var app = express();
-//var cors = require("cors")
+const express =require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/user');
+const Todo = require('./models/Todo');
 
-app.use(express.json());
-//app.use(cors());
-
-    app.get("/",function(req,res){
-        res.send("hello");
-    })
+mongoose.connect('mongodb+srv://sasini:kidfox98@cluster0.vlwi3.mongodb.net/ticktick')
+app.use(cors());
+app.use((bodyParser.json()));
+app.use(bodyParser.urlencoded({extended:false}));
 
 
-    app.listen(3001,function (){
-  console.log("listen to the port 3001")  
-});
+const port = process.env.PORT||5000;
+
+app.listen(port,(err)=>{
+  
+    if(err)return console.log(err);
+
+    console.log('server running on port: ',port)
+
+})
