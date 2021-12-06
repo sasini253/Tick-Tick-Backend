798b6ef882445f215e4c7ce73c7d59395f93fbae
Sasini Maheesha <72601860+sasini253@users.noreply.github.com>
2021-12-06 15:14:15 +0530

    Update Todo.js

--- models/Todo.js
+++ models/Todo.js
@@ -1,13 +1,1 @@
-const mongoose = require = require('mongoose');
-const Schema = mongoose.Schema;
 
-const todoSchema =new Schema({
-    task_id :String,
-    task:String
-    
-      
-})
-    
-    
-    const Todo = mongoose.model ('Todo', todoSchema);
-    module.exports = Todo
