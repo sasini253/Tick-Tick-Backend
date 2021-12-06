44c9d0fed09debb66f5fa353b9000e33e4789829
Sasini Maheesha <72601860+sasini253@users.noreply.github.com>
2021-12-06 15:12:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,3 +12,12 @@ app.use((bodyParser.json()));
 app.use(bodyParser.urlencoded({extended:false}));
 
 
+const port = process.env.PORT||5000;
+
+app.listen(port,(err)=>{
+  
+    if(err)return console.log(err);
+
+    console.log('server running on port: ',port)
+
+})
